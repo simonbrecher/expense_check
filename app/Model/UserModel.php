db7395c1894216f9d64841ce5cb88814783238e4
simonbrecher <brecher.simon@gmail.com>
2021-02-27 17:50:13 +0100

    Added user signing in (creating new family) Users can now sign in and create new family this way.

--- app/Model/UserModel.php
+++ app/Model/UserModel.php
@@ -7,15 +7,44 @@ use Nette;
 
 class UserModel extends BaseModel
 {
+    private $passwords;
+
     public function __construct(Nette\Database\Explorer $database, Nette\Security\User $user, Nette\Security\Passwords $passwords)
     {
         parent::__construct($database, $user);
         $this->passwords = $passwords;
     }
 
-    public function saveUser(array $values): void
+    public function saveUser(Nette\Utils\ArrayHash $values): void
     {
+        $database = $this->database;
 
+        $sameUsername = $database->table('user')->where('username', $values->username)->fetch();
+        if ($sameUsername) {
+            throw new DupliciteUserException('Uživatelské jméno už existuje.');
+        }
+        $sameEmail = $database->table('user')->where('email', $values->email)->fetch();
+        if ($sameEmail) {
+            throw new DupliciteUserException('Email je už zabraný.');
+        }
+
+        $data = array(
+            'name' => $values->name,
+            'surname' => $values->surname,
+            'username' => $values->username,
+            'email' => $values->email,
+            'password_hash' => $this->passwords->hash($values->password)
+        );
+
+        try {
+            $database->beginTransaction();
+                $family = $database->table('family')->insert([]);
+                $family->related('user')->insert($data);
+            $database->commit();
+        } catch (\PDOException $exception) {
+            $this->database->rollBack();
+            throw $exception;
+        }
     }
 }
 
--- app/Presenters/UserPresenter.php
+++ app/Presenters/UserPresenter.php
@@ -5,13 +5,19 @@ namespace App\Presenters;
 
 use App\Form\LoginForm;
 use App\Form\SigninForm;
-use Tracy\Debugger;
+
+use App\Model\DupliciteUserException;
+use Nette\Security\AuthenticationException;
+
+use App\Model;
 
 class UserPresenter extends BasePresenter
 {
-    public function renderDefault(): void
-    {
+    private $userModel;
 
+    public function __construct(Model\UserModel $userModel)
+    {
+        $this->userModel = $userModel;
     }
 
     public function actionLogin(): void
@@ -21,15 +27,59 @@ class UserPresenter extends BasePresenter
         }
     }
 
+    public function createComponentLoginForm(): LoginForm
+    {
+        $form = new LoginForm();
+
+        $form->addGroup('body');
+
+            $form->addText('username', 'Uživatelské jméno:')->setRequired('Zadejte prosím uživatelské jméno.');
+
+            $form->addPassword('password', 'Heslo:')->setRequired('Zadejte prosím heslo.');
+
+        $form->addGroup('buttons');
+
+            $form->addSubmit('submit', 'Přihlásit se');
+
+        $form->onSuccess[] = [$this, 'loginFormSuccess'];
+
+        return $form;
+    }
+
+    public function loginFormSuccess(LoginForm $form): void
+    {
+        $values = $form->values;
+
+        try {
+            $this->user->login($values->username, $values->password);
+
+            $this->redirect('User:');
+        } catch (AuthenticationException) {
+            $this->flashMessage('Nesprávné uživatelské jméno nebo heslo.', 'error');
+        }
+    }
+
     public function createComponentSigninForm(): SigninForm
     {
         $form = new SigninForm();
 
-        $form->addText('username', 'Uživatelské jméno:')->setRequired('Zadejte prosím uživatelské jméno.');
+        $form->addGroup('body');
 
-        $form->addPassword('password', 'Heslo:')->setRequired('Zadejte prosím heslo.');
+            $form->addText('name', 'Jméno:')->setRequired('Zadejte prosím své jméno.');
 
-        $form->addSubmit('submit', 'Přihlásit se');
+            $form->addText('surname', 'Příjmení:')->setRequired('Zadejte prosím své příjmení.');
+
+            $form->addText('username', 'Uživatelské jméno:')->setRequired('Zadejte prosím uživatelské jméno.');
+
+            $form->addEmail('email', 'Email:')->setRequired('Zadejte prosím email.');
+
+            if (!$this->user->isLoggedIn()) {
+                $form->addPassword('password', 'Heslo:')->setRequired('Zadejte prosím heslo.');
+            }
+
+        $form->addGroup('buttons');
+
+            $form->addSubmit('submit', 'Registrovat se');
 
         $form->onSuccess[] = [$this, 'signinFormSuccess'];
 
@@ -39,15 +89,16 @@ class UserPresenter extends BasePresenter
     public function signinFormSuccess(SigninForm $form): void
     {
         $values = $form->values;
-        $this->user->login($values->username, $values->password);
 
-        $this->redirect('User:');
-    }
+        try {
+            $this->userModel->saveUser($values);
+            $this->flashMessage('Uživatelský účet byl úspěšně vytvořený. Můžete se přihlásit.', 'success');
 
-    public function createComponentLoginForm(): LoginForm
-    {
-        $form = new LoginForm();
-
-        return $form;
+            $this->redirect('User:');
+        } catch (DupliciteUserException $exception) {
+            $this->flashMessage($exception->getMessage(), 'error');
+        } catch (\PDOException $exception) {
+            $this->flashMessage('Uživatelský účet se nepodařilo vytvořit.', 'error');
+        }
     }
 }
